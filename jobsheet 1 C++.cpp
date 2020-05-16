a2cac1847f857f1f1a801e84c12292b573f9f360
Zulhan Ozi Firmanu <64623575+019148@users.noreply.github.com>
2020-05-16 12:25:19 +0800

    Create jobsheet 1 C++.cpp
    Buatlah program array satu dimensi yang menampilkan NIM masing-masing mahasiswa

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Nama: Zulhan Ozi Firmanu"<<endl;
+	cout<<"NIM: F1b019148"<<endl;
+	cout<<"Kelompok: 28"<<endl;
+
+	int my_array [8] = {3,4,5,6,7,2,9,8};
+	
+	cout << ("Data ke-1 : ",my_array[0]);
+	cout << ("Data ke-2 : ",my_array[1]);
+	cout << ("Data ke-3 : ",my_array[2]);
+	cout << ("Data ke-4 : ",my_array[3]);
+	cout << ("Data ke-5 : ",my_array[4]);
+	cout << ("Data ke-6 : ",my_array[5]);
+	cout << ("Data ke-7 : ",my_array[6]);
+	cout << ("Data ke-8 : ",my_array[7]);
+}
