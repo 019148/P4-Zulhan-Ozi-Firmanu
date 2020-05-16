bac258ebdde2d8edd10fb24a93c6b19479690c18
Zulhan Ozi Firmanu <64623575+019148@users.noreply.github.com>
2020-05-16 13:02:01 +0800

    Create jobsheet 2 C++.cpp
    Buatlah Program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap mahasiswa

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<< "Nama : Zulhan Ozi Firmanu"<<endl;
+	cout<< "NIM : F1B019148"<<endl;
+	cout<< "Kelompok : 28"<<endl;
+int x [8];
+	cout<<"Masukkan nilai 1 : ";
+	cin>>x[0];
+	cout<<"Masukkan nilai 2 : ";
+	cin>>x[1];
+	cout<<"Masukkan nilai 3 : ";
+	cin>>x[2];
+	cout<<"Masukkan nilai 4 : ";
+	cin>>x[3];
+	cout<<"Masukkan nilai 5 : ";
+	cin>>x[4];
+	cout<<"Masukkan nilai 6 : ";
+	cin>>x[5];
+	cout<<"Masukkan nilai 7 : ";
+	cin>>x[6];
+	cout<<"Masukkan nilai 8 : ";
+	cin>>x[7];
+	cout<<"Nilai ke-1 = "<<x[0]<<endl;
+	cout<<"Nilai ke-2 = "<<x[1]<<endl;
+	cout<<"Nilai ke-3 = "<<x[2]<<endl;
+	cout<<"Nilai ke-4 = "<<x[3]<<endl;
+	cout<<"Nilai ke-5 = "<<x[4]<<endl;
+	cout<<"Nilai ke-6 = "<<x[5]<<endl;
+	cout<<"Nilai ke-7 = "<<x[6]<<endl;
+	cout<<"Nilai ke-8 = "<<x[7]<<endl;
+	
+}
